7d3affb113b190c43497866209e8e695c3113f8f
Abhimanue Rajesh <abhimanuemvk@gmail.com>
2025-07-23 23:32:21 +0530

    added the message framework

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 # from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+
 from .forms import RegisterForm
 
 
@@ -8,3 +11,15 @@ class SignUpView(CreateView):
     form_class = RegisterForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+class CustomLoginView(LoginView):
+    def form_valid(self, form):
+        messages.success(self.request, f"Welcome back, {form.get_user().username}!")
+        print(f"User {form.get_user().username} logged in successfully.")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Invalid username or password. Please try again.")
+        print("Login attempt failed.")
+        return super().form_invalid(form)
